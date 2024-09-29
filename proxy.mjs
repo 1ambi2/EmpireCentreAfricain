066ac736d3554cc7a165113330b5c502e52fbdff
Ambroise <ambroise.dussert@gmail.com>
2024-09-29 15:47:18 +0200

    basic

---
+++ proxy.mjs
@@ -0,0 +1,43 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // Pour parser les requêtes JSON
+const port = 3000;
+
+app.post("/set-end-url", (req, res) => {
+  const { newURL } = req.body;
+  if (newURL) {
+    setEndURL(newURL);
+    res.status(200).send("URL updated successfully");
+  } else {
+    res.status(400).send("Invalid URL");
+  }
+});
+
+app.get("/proxy", async (req, res) => {
+  const endURL = req.query.endURL || "";
+  const apiUrl = "https://publicapi.nationsglory.fr/" + endURL;
+  const bearerToken =
+    "NGAPI_Tnlcbrrl1N50YxAkNmPFfjl(ANBelWsV4e7267d4d816cf353932b759f3fcda39";
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server running at http://localhost:${port}`);
+});
---
+++ static/js/api.js
@@ -0,0 +1,31 @@
+const proxyUrl =
+  "http://127.0.0.1:3000/proxy?endURL=country/lime/EmpireCentreAfricain";
+
+fetch(proxyUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    //Power
+    const power = data.power;
+    const maxPower = data.maxpower;
+    const powerElement = document.querySelector(".power");
+    powerElement.innerHTML = power + "/" + maxPower;
+
+    //Claims
+    const claims = data.count_claims;
+    const claimsElement = document.querySelector(".claim");
+    claimsElement.innerHTML = claims;
+
+    //MMR
+    const mmr = data.mmr;
+    const mmrElement = document.querySelector(".mmr");
+    mmrElement.innerHTML = mmr;
+
+    //Level
+    const level = data.level;
+    const levelElement = document.querySelector(".level");
+    levelElement.innerHTML = level;
+
+  })
+  .catch((err) => console.error(err));
